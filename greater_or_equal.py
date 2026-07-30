05d24aa6e517f1035606e08e29caa0b1cabb98d6
Chetan <chetangola2025@gmail.com>
2026-07-30 21:47:07 +0530

    Add files via upload

---
+++ greater_or_equal.py
@@ -0,0 +1,10 @@
+# Write a program to input two integer numbers, a and b. 
+# Print 'True' if a is greater than or equal to b. If not print 'False'.
+
+
+a = int(input("Enter your first number : "))
+b = int(input("Enter your second number : "))
+if(a>=b):
+    print("True")
+else:
+    print("False")
